24624ba094c9408c6a27493a7d8fffa1170810cc
Kevin Christianto <christiantok12@gmail.com>
2022-08-30 10:06:06 +0700

    Added modal section

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -190,6 +190,8 @@
         <i class="fas fa-angle-up"></i>
     </a>
 
+    @yield('modal-section')
+
     <!-- Bootstrap core JavaScript-->
     <script src="{{ asset('assets/vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
